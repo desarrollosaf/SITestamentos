df5e2e1cbd83ef80580addb894f1f9fbcca7e014
Nahum <flashjzd58@gmail.com>
2025-06-13 17:18:12 -0600

    cambio controlador

--- backend/src/controllers/solicitud.ts
+++ backend/src/controllers/solicitud.ts
@@ -3,7 +3,10 @@ import { Solicitud } from '../models/solicitud';
 import User from '../models/user';
 import bcrypt from 'bcrypt'
 import { Testigo } from '../models/testigos';
+import sequelizefun from '../database/fun'; // La conexión
+import { dp_datospersonales } from '../models/fun/dp_datospersonales';
 
+dp_datospersonales.initModel(sequelizefun);
 
 export const saveinfo = async (req: Request, res: Response): Promise<any> => {
     const { data } = req.body;
@@ -13,9 +16,33 @@ export const saveinfo = async (req: Request, res: Response): Promise<any> => {
     const UpasswordHash = await bcrypt.hash(Upassword, 10);
     const newUser = await User.create({
       name: data.curp,
-      email: data.curp,
+      email: data.correo_per,
       password: UpasswordHash,
     });
+
+    let registro = await dp_datospersonales.findOne({ 
+        where: { f_curp: data.curp }
+    });
+
+    if (!registro) {
+         const test = await dp_datospersonales.create({
+             f_curp: data.curp,
+             f_rfc: data.rfc,
+             f_nombre: data.nombre,
+             f_primer_apellido: data.f_primer_apellido,
+             f_segundo_apellido: data.f_segundo_apellido,
+             f_fecha_nacimiento: data.f_fecha_nacimiento,
+             estado_id: data.estado_id,
+             municipio_id: data.municipio_id,
+             colonia_id: data.colonia_id,
+             f_domicilio: data.f_domicilio,
+             numext: data.numext,
+             correo_per: data.correo_per,
+             f_homclave: ''
+         });    
+    };
+
+
 
     const files = req.files as {
         [fieldname: string]: Express.Multer.File[];
