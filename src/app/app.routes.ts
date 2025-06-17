54090b52c580f6a12d100725f39c1772303442b5
MarSGlez <martin.sanchezg@hotmail.com>
2025-06-17 13:09:07 -0600

    cambios home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { BaseComponent } from './views/layout/base/base.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./views/pages/home/home.component').then(c => c.HomeComponent) 
+  },
   { path: 'auth', loadChildren: () => import('./views/pages/auth/auth.routes')},
   {
     path: '',
--- src/app/views/pages/auth/login/login.component.ts
+++ src/app/views/pages/auth/login/login.component.ts
@@ -35,7 +35,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/solicitudes';
 
   }
 
