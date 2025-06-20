f6fb9e904adf116c57281fab3a763f008dd2cc58
cescDG <cesar199403@hotmail.com>
2025-06-20 15:05:07 -0600

    form

--- backend/src/database/testamentosConnection.ts
+++ backend/src/database/testamentosConnection.ts
@@ -13,8 +13,8 @@
 
 import { Sequelize } from "sequelize"
 
-const sequelizeTestamentos = new Sequelize('adminplem_testamentos', 'usr_testamentos', '8lv8EXLKNvCovs2tx4MF', {
-    host: '192.168.36.53',
+const sequelizeTestamentos = new Sequelize('testamentos', 'root', '', {
+    host: 'localhost',
     dialect: 'mysql',
     define: {
         freezeTableName: true 
