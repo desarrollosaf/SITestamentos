d8957d809944cd9d31a2878627c9c9b93273cf5a
MarSGlez <martin.sanchezg@hotmail.com>
2025-06-24 14:38:17 -0600

    cambios reportes

--- backend/dist/routes/reportes.js
+++ backend/dist/routes/reportes.js
@@ -4,5 +4,5 @@ const express_1 = require("express");
 const reportes_1 = require("../controllers/reportes");
 const router = (0, express_1.Router)();
 router.get("/api/reporte/getinfo/", reportes_1.getinfo);
-router.get("/api/reporte/getregistros/", reportes_1.getregistros);
+router.post("/api/reporte/getregistros/", reportes_1.getregistros);
 exports.default = router;
