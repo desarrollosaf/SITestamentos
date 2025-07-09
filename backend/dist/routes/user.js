273d8c9740c950e78115f699f8cf10a99395b9b6
Nahum <flashjzd58@gmail.com>
2025-07-09 11:05:16 -0600

    cambios funcion de validacion

--- backend/dist/routes/user.js
+++ backend/dist/routes/user.js
@@ -5,4 +5,5 @@ const users_1 = require("../controllers/users");
 const router = (0, express_1.Router)();
 router.post("/api/user/login", users_1.LoginUser);
 router.get("/api/user/getusers", users_1.ReadUser);
+router.get('/api/user/me', users_1.getCurrentUser);
 exports.default = router;
