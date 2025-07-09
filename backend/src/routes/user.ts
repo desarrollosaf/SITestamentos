fdd0d989095b87bd8f2943e8bf994f6c1b87010e
Nahum <flashjzd58@gmail.com>
2025-07-09 13:07:00 -0600

    cambios cerrar sesion con token

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getCurrentUser, LoginUser, ReadUser } from "../controllers/users";
+import { getCurrentUser, LoginUser, cerrarsesion, ReadUser } from "../controllers/users";
 import { verifyToken } from "../middlewares/auth";
 
 const router = Router();
-router.post("/api/user/login", LoginUser)
-router.get("/api/user/getusers", ReadUser)
-router.get('/api/user/me', verifyToken, getCurrentUser);
 
-export default router
+router.post("/api/user/login", LoginUser);
+router.get("/api/user/getusers", ReadUser);
+router.get("/api/user/me", verifyToken, getCurrentUser);
+router.post('/api/user/cerrarsesion',  cerrarsesion);
+
+export default router;
